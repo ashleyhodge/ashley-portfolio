ec79bbaf8799e1c383a23f267afc62fee24a75af
ashleyhodge <ashhodge@comcast.net>
2022-07-28 12:29:29 -0400

    deploy

---
+++ server.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQamNszVEAaWBlVCcx916vPUawp0BUbuI",
+  authDomain: "ashley-portfolio-ce6fa.firebaseapp.com",
+  projectId: "ashley-portfolio-ce6fa",
+  storageBucket: "ashley-portfolio-ce6fa.appspot.com",
+  messagingSenderId: "2139126979",
+  appId: "1:2139126979:web:fc73e5dbf755b8a727b676",
+  measurementId: "G-W1S27F60PN"
+};
+
+const app = initializeApp(firebaseConfig);
