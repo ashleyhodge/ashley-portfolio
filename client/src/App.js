2c949ddc7e59b8837b719c0605bdd759dcf330a4
ashleyhodge <ashhodge@comcast.net>
2023-02-14 16:07:07 -0500

    redo front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Nav from "./components/Nav";
-import Home from "./pages/Home";
-import Portfolio from "./pages/Portfolio";
-import About from "./pages/About";
-import Resume from "./pages/Resume";
-import Contact from "./pages/Contact";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Nav />
-        <Routes>
-          <Route
-            path="/ashley-portfolio"
-            element={<Home />}
-          />
-          <Route 
-            path="/portfolio"
-            element={<Portfolio />}
-          />
-          <Route
-            path="/about"
-            element={<About />}
-          />
-          <Route
-            path="/resume"
-            element={<Resume />}
-          />
-          <Route
-            path="/contact"
-            element={<Contact />}
-          />
-        </Routes>
-      </div>
-    </Router>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
