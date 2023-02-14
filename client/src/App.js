bcb8ca507ad886700c6371694095a90c68fc48a8
ashleyhodge <ashhodge@comcast.net>
2023-02-14 18:11:53 -0500

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+import Home from './pages/Home';
+import Nav from './components/Nav';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ApolloProvider client={client}>
+      <div>
+        <Router>
+          <main>
+            <Nav />
+            <Routes>
+              <Route 
+                path='/'
+                element={<Home />}
+              />
+            </Routes>
+          </main>
+        </Router>
+      </div>
+    </ApolloProvider>
+  )
 }
 
 export default App;
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,76 @@
+import { useState } from "react"
+import { Link } from "react-router-dom";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"
+
+
+const Nav = () => {
+const [showNav, setShowNav] = useState(false);
+const navToggle = () => setShowNav(!showNav);
+
+  return(
+    <header>
+      <nav>
+        {/* Desktop Navigation */}
+        <div className="hidden sm:block">
+          <ul className="flex m-[20px] justify-around">
+              <li className="nav">
+                home
+              </li>
+              <li className="nav">
+              about
+              </li>
+              <li className="nav">
+                projects
+              </li>
+              <li className=" nav">
+              resume
+            </li>
+              <li className=" nav">
+              contact
+            </li>
+          </ul>
+        </div>
+        {/* Mobile Navigation */}
+        <div>
+              
+            <div className="m-3">
+                <AiOutlineMenu
+                onClick={navToggle}
+                className="sm:hidden bg-[#D44416] p-3 rounded-full opacity-[50%] text-white"
+                size={60}
+              />
+              <ul className={
+                (showNav ? "top-0" : "-top-full") +
+                " absolute transition-all duration-[1.0s] ease-in-out w-full h-full z-10 bg-white right-0  sm:hidden"
+              }>
+              <AiOutlineClose
+              onClick={navToggle}
+              size={60}
+              className="bg-[#D44416] p-3 rounded-full opacity-[50%] text-white m-3"
+              />
+              <div className="flex flex-col items-center pt-14">
+                <li className="nav-mobile">
+                  home
+                </li>
+                <li className="nav-mobile">
+                about
+                </li>
+                <li className="nav-mobile">
+                  projects
+                </li>
+                <li className="nav-mobile">
+                resume
+                </li>
+                <li className="nav-mobile">
+                contact
+                </li>
+              </div>
+              </ul>
+            </div>
+          </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav;
